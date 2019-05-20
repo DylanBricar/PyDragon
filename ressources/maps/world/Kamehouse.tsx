e6107de0a16b415447128f94c0ca6c4ee23eb9ad
Naruhiko <Dylan3250@hotmail.com>
2019-05-20 23:03:49 +0200

    Ajout d'une nouvelle carte

---
+++ ressources/maps/world/Kamehouse.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Kamehouse" tilewidth="32" tileheight="32" tilecount="384" columns="12">
+ <image source="ressources/maps/world/Kamehouse.png" trans="baff69" width="384" height="1024"/>
+</tileset>
---
+++ ressources/maps/world/alltiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="alltiles" tilewidth="32" tileheight="32" tilecount="4018" columns="14">
+ <image source="ressources/maps/world/tiles.png" trans="baff69" width="448" height="9184"/>
+</tileset>
